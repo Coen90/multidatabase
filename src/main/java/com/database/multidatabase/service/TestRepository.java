6a34d230af0643b7fe551cc08ae1d31d361e48dc
Coen90 <81370558+Coen90@users.noreply.github.com>
2022-10-17 09:24:06 +0900

    Delete TestRepository.java

--- src/main/java/com/database/multidatabase/service/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.database.multidatabase.service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TestRepository extends CrudRepository {
-}
